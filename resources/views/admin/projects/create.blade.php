689ec086a299d26785908f2beed02976e9e32e25
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-08-30 15:58:10 +0200

    add selected types in create and edit CRUD

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,6 +17,22 @@
                     <input type="text" class="form-control" id="title" placeholder="Insert your post's title" name="title"  value="{{ old('title', '') }}">
                 </div>
 
+                @error('type_id')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <div class="mb-5">
+                    <label for="type_id" class="form-label">
+                        Type
+                    </label>
+                    <select class='form-select' name="type_id" id="type_id">
+                        @foreach ($types as $type)
+                            <option value="{{ $type->id }}" {{ old('type_id') == $type->id ? 'selected' : '' }}>
+                                {{ $type->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
                 @error('image')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
